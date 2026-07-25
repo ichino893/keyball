0f019d569d0e2a01bc069d3a4993e20f3b1c6c8b
ichino893 <ichi3h@gmail.com>
2026-07-25 18:01:24 +0900

    変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -22,52 +22,53 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // トラックボール・スクロール感度の設定
 #ifndef KEYBALL_CPI_DEFAULT
-#    define KEYBALL_CPI_DEFAULT 1200  // 通常のカーソル速度（初期値は900程度。数字を大きくすると速くなります）
+#    define KEYBALL_CPI_DEFAULT 2000  // 通常のカーソル速度
 #endif
 
 #ifndef KEYBALL_SCROLL_DIVIDER
-#    define KEYBALL_SCROLL_DIVIDER 4  // スクロール感度（初期値は1〜2程度。数字を大きくすると移動量が小さく/緩やかになります）
+#    define KEYBALL_SCROLL_DIVIDER 8  // スクロール感度
 #endif
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                         KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                         KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
     KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
   ),
 
   [1] = LAYOUT_universal(
-    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
-    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
-    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
+    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
+    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                                        S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
+    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                                         KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
     KC_INT1  , KC_EQL   , S(KC_3)  , _______  , _______  , _______  ,      TO(2)    , TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
   ),
 
   [2] = LAYOUT_universal(
-    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
-   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
-    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
+    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                                         KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
+   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                        S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
+    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                                       S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
     KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                                         _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                                         _______  , _______  , _______  , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                                         CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
     QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
   ),
 };
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    // Layer 1（左手親指スペース長押し）または Layer 3（設定画面）の時にスクロールモードを有効化
+    uint8_t layer = get_highest_layer(state);
+    keyball_set_scroll_mode(layer == 1 || layer == 3);
     return state;
 }
- 
+
 #ifdef OLED_ENABLE
 
 #    include "lib/oledkit/oledkit.h"
@@ -90,7 +91,6 @@ const uint16_t PROGMEM my_btn2[]   = {KC_I, KC_O, COMBO_END};     // I + O
 const uint16_t PROGMEM my_bspc[]   = {KC_O, KC_P, COMBO_END};     // O + P
 const uint16_t PROGMEM my_p_mins[] = {KC_P, KC_MINS, COMBO_END}; // P + (Pの下)
 
-// --- 追加分 ---
 const uint16_t PROGMEM my_left[]   = {KC_J, KC_K, COMBO_END};     // J + K
 const uint16_t PROGMEM my_up[]     = {KC_K, KC_I, COMBO_END};     // K + I
 const uint16_t PROGMEM my_right[]  = {KC_K, KC_L, COMBO_END};     // K + L
@@ -100,6 +100,10 @@ const uint16_t PROGMEM my_pgup[]   = {KC_M, KC_COMM, COMBO_END};  // M + ,
 const uint16_t PROGMEM my_pgdn[]   = {KC_COMM, KC_DOT, COMBO_END}; // , + .
 const uint16_t PROGMEM my_end[]    = {KC_COMM, KC_SLSH, COMBO_END};// , + /
 
+// --- 今回の追加分 ---
+const uint16_t PROGMEM my_sft[]    = {KC_Z, KC_X, COMBO_END};     // Z + X
+const uint16_t PROGMEM my_tab[]    = {KC_L, KC_MINS, COMBO_END};  // L + -
+
 // 2. 組み合わせと出力するキーの割り当て
 combo_t key_combos[] = {
     COMBO(my_esc, KC_ESC),       // Q + W         -> ESC
@@ -108,7 +112,6 @@ combo_t key_combos[] = {
     COMBO(my_bspc, KC_BSPC),     // O + P         -> BackSpace
     COMBO(my_p_mins, KC_DEL),    // P + (Pの下)  -> Delete
 
-    // --- 追加分 ---
     COMBO(my_left, KC_LEFT),     // J + K         -> ← (左)
     COMBO(my_up, KC_UP),         // K + I         -> ↑ (上)
     COMBO(my_right, KC_RGHT),    // K + L         -> → (右)
@@ -117,5 +120,9 @@ combo_t key_combos[] = {
     COMBO(my_pgup, KC_PGUP),     // M + ,         -> PageUp
     COMBO(my_pgdn, KC_PGDN),     // , + .         -> PageDown
     COMBO(my_end, KC_END),       // , + /         -> END
+
+    // --- 今回の追加分 ---
+    COMBO(my_sft, KC_LSFT),      // Z + X         -> Shift
+    COMBO(my_tab, KC_TAB),       // L + -         -> Tab
 };
 #endif
