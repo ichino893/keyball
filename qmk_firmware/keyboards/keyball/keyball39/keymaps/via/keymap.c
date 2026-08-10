77acc050f23508576432c7d37bb3f4e9541029bf
ichino893 <ichi3h@gmail.com>
2026-08-11 00:20:13 +0900

    20260811

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -86,19 +86,19 @@ void oledkit_render_info_user(void) {
 const uint16_t PROGMEM my_esc[]    = {KC_Q, KC_E, COMBO_END};     // Q + E (※ESCはQEのまま残しています)
 const uint16_t PROGMEM my_btn1[]   = {KC_J, KC_I, COMBO_END};     // J + I (元に戻しました)
 const uint16_t PROGMEM my_btn2[]   = {KC_I, KC_O, COMBO_END};     // I + O (元に戻しました)
-const uint16_t PROGMEM my_bspc[]   = {KC_O, KC_P, COMBO_END};     // O + P
+// const uint16_t PROGMEM my_bspc[]   = {KC_O, KC_P, COMBO_END};     // O + P
 const uint16_t PROGMEM my_p_mins[] = {KC_P, KC_MINS, COMBO_END}; // P + (Pの下)
 
-const uint16_t PROGMEM my_left[]   = {KC_J, KC_K, COMBO_END};     // J + K (元に戻しました)
-const uint16_t PROGMEM my_up[]     = {KC_K, KC_I, COMBO_END};     // K + I (元に戻しました)
-const uint16_t PROGMEM my_right[]  = {KC_K, KC_L, COMBO_END};     // K + L (元に戻しました)
-const uint16_t PROGMEM my_down[]   = {KC_K, KC_COMM, COMBO_END};  // K + , (元に戻しました)
-const uint16_t PROGMEM my_home[]   = {KC_N, KC_M, COMBO_END};     // N + M
-const uint16_t PROGMEM my_pgup[]   = {KC_M, KC_COMM, COMBO_END};  // M + ,
-const uint16_t PROGMEM my_pgdn[]   = {KC_COMM, KC_DOT, COMBO_END}; // , + .
-const uint16_t PROGMEM my_end[]    = {KC_COMM, KC_SLSH, COMBO_END};// . + /
+const uint16_t PROGMEM my_left[]   = {KC_J, KC_K, COMBO_END};     // M + , (元に戻しました)
+const uint16_t PROGMEM my_up[]     = {KC_K, KC_I, COMBO_END};     // K + , (元に戻しました)
+const uint16_t PROGMEM my_right[]  = {KC_K, KC_L, COMBO_END};     // , + . (元に戻しました)
+const uint16_t PROGMEM my_down[]   = {KC_K, KC_COMM, COMBO_END};  // . + / (元に戻しました)
+// const uint16_t PROGMEM my_home[]   = {KC_N, KC_M, COMBO_END};     // N + M
+// const uint16_t PROGMEM my_pgup[]   = {KC_M, KC_COMM, COMBO_END};  // M + ,
+// const uint16_t PROGMEM my_pgdn[]   = {KC_COMM, KC_DOT, COMBO_END}; // , + .
+// const uint16_t PROGMEM my_end[]    = {KC_COMM, KC_SLSH, COMBO_END};// . + /
 
-const uint16_t PROGMEM my_sft[]    = {KC_Z, KC_X, COMBO_END};     // Z + X
+// const uint16_t PROGMEM my_sft[]    = {KC_Z, KC_X, COMBO_END};     // Z + X
 const uint16_t PROGMEM my_ent[]    = {KC_L, KC_MINS, COMBO_END};  // L + -
 const uint16_t PROGMEM my_tab[]    = {KC_R, KC_T, COMBO_END};     // R + T
 
@@ -107,18 +107,18 @@ combo_t key_combos[] = {
     COMBO(my_esc, KC_ESC),       // Q + E          -> ESC
     COMBO(my_btn1, KC_BTN1),     // J + I          -> マウス左クリック
     COMBO(my_btn2, KC_BTN2),     // I + O          -> マウス右クリック
-    COMBO(my_bspc, KC_BSPC),     // O + P          -> BackSpace
+    // COMBO(my_bspc, KC_BSPC),     // O + P          -> BackSpace
     COMBO(my_p_mins, KC_DEL),    // P + (Pの下)  -> Delete
 
-    COMBO(my_left, KC_LEFT),     // J + K          -> ← (左)
-    COMBO(my_up, KC_UP),         // K + I          -> ↑ (上)
-    COMBO(my_right, KC_RGHT),    // K + L          -> → (右)
-    COMBO(my_down, KC_DOWN),     // K + ,          -> ↓ (下)
-    COMBO(my_home, KC_HOME),     // N + M          -> HOME
-    COMBO(my_pgup, KC_PGUP),     // M + ,          -> PageUp
-    COMBO(my_pgdn, KC_PGDN),     // , + .          -> PageDown
-    COMBO(my_end, KC_END),       // . + /          -> END
-    COMBO(my_sft, KC_LSFT),      // Z + X          -> Shift
+    COMBO(my_left, KC_LEFT),     // M + ,          -> ← (左)
+    COMBO(my_up, KC_UP),         // K + ,          -> ↑ (上)
+    COMBO(my_right, KC_RGHT),    // , + .          -> → (右)
+    COMBO(my_down, KC_DOWN),     // . + /          -> ↓ (下)
+    // COMBO(my_home, KC_HOME),     // N + M          -> HOME
+    // COMBO(my_pgup, KC_PGUP),     // M + ,          -> PageUp
+    // COMBO(my_pgdn, KC_PGDN),     // , + .          -> PageDown
+    // COMBO(my_end, KC_END),       // . + /          -> END
+    // COMBO(my_sft, KC_LSFT),      // Z + X          -> Shift
     COMBO(my_ent, KC_ENT),       // L + -          -> Enter
     COMBO(my_tab, KC_TAB),       // R + T          -> Tab
 };
