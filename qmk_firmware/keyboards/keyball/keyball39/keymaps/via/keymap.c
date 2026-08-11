97c2224c4d82a4a96195b657ad16677a2cbe7687
ichino893 <ichi3h@gmail.com>
2026-08-11 14:32:13 +0900

    20260811-5

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -31,45 +31,44 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                                 KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                                 KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                                 KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                                   KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                                   KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                                   KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
     KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, MO(2)
   ),
 
   [1] = LAYOUT_universal(
-    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                                                 KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
-    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                                                 S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
-    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                                                 KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
+    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                                                   KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
+    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                                                   S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
+    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                                                   KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
     KC_INT1  , KC_EQL    , S(KC_3)  , _______  , _______  , _______  ,      TO(2)    , TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
   ),
 
   [2] = LAYOUT_universal(
-    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                                                 KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
-   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                                 S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
-    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                                                 S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
+    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                                                   KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
+   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                                   S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
+    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                                                   S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
     KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                                                 _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                                                 _______  , _______  , _______  , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                                                 CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                                                   _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                                                   _______  , _______  , _______  , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                                                   CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
     QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
   ),
 };
 // clang-format on
 
-// レイヤー制御：Layer 2 の時だけスクロールモードを有効化
+// レイヤー制御：標準のスクロールモード呼び出しを無効化（二重処理を防止）
 layer_state_t layer_state_set_user(layer_state_t state) {
-    bool is_layer_2 = (get_highest_layer(state) == 2);
-    keyball_set_scroll_mode(is_layer_2);
     return state;
 }
 
-// ▼ 縦横両対応のスクロール制御処理（端数保持版） ▼
+// ▼ 縦横両対応のスクロール制御処理 ▼
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (keyball_get_scroll_mode()) {
+    // 現在の最上位レイヤーが Layer 2 かどうかを判定
+    if (get_highest_layer(layer_state) == 2) {
         static int16_t scroll_x_remainder = 0;
         static int16_t scroll_y_remainder = 0;
 
@@ -79,19 +78,18 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 
         // スクロール量の計算（水平: h, 垂直: v）
         mouse_report.h = total_x / KEYBALL_SCROLL_DIVIDER;
-        mouse_report.v = -total_y / KEYBALL_SCROLL_DIVIDER; // マイナスで移動方向を調整
+        mouse_report.v = -total_y / KEYBALL_SCROLL_DIVIDER; // マイナスで向きを調整
 
-        // 12で割り切れなかった余りを保存し、次回へ繰り越し
+        // 余りを保存し次回に繰り越し
         scroll_x_remainder = total_x % KEYBALL_SCROLL_DIVIDER;
         scroll_y_remainder = total_y % KEYBALL_SCROLL_DIVIDER;
 
-        // マウスポインタ自体の移動を停止
+        // カーソル移動を停止
         mouse_report.x = 0;
         mouse_report.y = 0;
     }
     return mouse_report;
 }
-
 
 #ifdef OLED_ENABLE
 
